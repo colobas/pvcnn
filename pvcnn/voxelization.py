fix normalization

pvcnn/voxelization.py
@@ -17,16 +17,15 @@ class Voxelization(nn.Module):
 
     def forward(self, features, coords):
         coords = coords.detach()
-        norm_coords = coords - coords.mean(2, keepdim=True)
+        norm_coords = coords - coords.mean(dim=1, keepdim=True)
         if self.normalize:
             norm_coords = norm_coords / (norm_coords.norm(dim=1, keepdim=True).max(dim=2, keepdim=True).values * 2.0 + self.eps) + 0.5
         else:
             norm_coords = (norm_coords + 1) / 2.0
-        breakpoint()
 
-        norm_coords[0] = torch.clamp(norm_coords[0] * self.rx, 0, self.rx - 1)
-        norm_coords[1] = torch.clamp(norm_coords[1] * self.ry, 0, self.ry - 1)
-        norm_coords[2] = torch.clamp(norm_coords[2] * self.rz, 0, self.rz - 1)
+        norm_coords[:, :, 0] = torch.clamp(norm_coords[:, :, 0] * self.rx, 0, self.rx - 1)
+        norm_coords[:, :, 1] = torch.clamp(norm_coords[:, :, 1] * self.ry, 0, self.ry - 1)
+        norm_coords[:, :, 2] = torch.clamp(norm_coords[:, :, 2] * self.rz, 0, self.rz - 1)
 
         vox_coords = torch.round(norm_coords).to(torch.int32)
         return F.avg_voxelize(features, vox_coords, self.rx, self.ry, self.rz), norm_coords